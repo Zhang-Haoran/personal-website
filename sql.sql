92b6b964f0e7190ad55b22f25745cb0530e4c2a0
Zhang-Haoran <haoranzhang001@gmail.com>
2021-10-05 17:58:07 +1100

    updated sql

--- sql.sql
+++ sql.sql
@@ -1,1 +1,7 @@
-create table blogs (id varchar not null constraint id_pk primary key, title varchar, content varchar, created_time varchar, updated_time varchar);
+create table blogs (
+    id SERIAL PRIMARY KEY,
+    title varchar,
+    content varchar,
+    created_time varchar, 
+    updated_time varchar
+    );
